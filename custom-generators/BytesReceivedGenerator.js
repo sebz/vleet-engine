fd89d7aed81502dddeb3a2e15bbdd2643392793f
Robert jacolin <rjacolin@sierrawireless.com>
2018-02-22 14:40:54 +0100

    Use parameters

--- custom-generators/BytesReceivedGenerator.js
+++ custom-generators/BytesReceivedGenerator.js
@@ -1,7 +1,16 @@
-function BytesReceivedGenerator() {}
+var chance = require("chance").Chance();
+
+function BytesReceivedGenerator(options) {
+	this.min = options.min;
+	this.max = options.max;
+}
 
 BytesReceivedGenerator.prototype.generate = function() {
-    return Math.round((Math.random() * 1000) / 2);
+	return chance.integer({
+		min: this.min,
+		max: this.max
+	});
+	//Math.round((Math.random() * 1000) / 2);
 };
 
 module.exports = BytesReceivedGenerator;
--- custom-generators/DurationGenerator.js
+++ custom-generators/DurationGenerator.js
@@ -1,11 +1,23 @@
 var chance = require("chance").Chance();
 
-function DurationGenerator() {}
+function DurationGenerator(options) {
+	this.mode = "generic";
+	if (options) {
+		this.min = options.min;
+		this.max = options.max;
+		this.mode = "custom";
+	}
+}
 
 DurationGenerator.prototype.generate = function() {
-	return chance.integer({
-		min: 1,
-		max: 24 * 60 * 60 * 1000
+	if (this.mode == "generic")
+		return chance.integer({
+			min: 1,
+			max: 24 * 60 * 60
+		});
+	else return chance.integer({
+		min: this.min,
+		max: this.max
 	});
 };
 
